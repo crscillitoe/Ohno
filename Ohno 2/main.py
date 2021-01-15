d450f8b14443aaae6a42539133e9a852b40e0af3
Christian Scillitoe <christian.scillitoe@gmail.com>
2021-01-15 00:56:36 +0000

    Oh no 2

---
+++ Ohno 2/main.py
@@ -0,0 +1,50 @@
+LetTerS = ["o", "h", "n"]
+
+
+def mY_GoOd_fUNCtiON(a, b, c, d, e, f, g) -> int:
+    print(
+        (
+            a[0].title()
+            + b[1].upper().lower().upper().lower()
+            + " ".upper().lower()
+            + g[
+                int(
+                    2
+                    + (
+                        (
+                            4
+                            - 2 * 23
+                            + 92 / 321 ** 2
+                            + 32 * 2
+                            + 1
+                            - 34
+                            + 3289312823
+                            - 23829 * 23
+                        )
+                        * 0
+                    )
+                )
+            ]
+            + a[
+                (
+                    132
+                    + 32
+                    + 4
+                    + 3
+                    + 212
+                    + 32812
+                    + 389221
+                    + 3299221
+                    + 32818231
+                    + 58912993128392183
+                    + 2381828283
+                )
+                * 0
+            ]
+        )
+    )
+    return -40
+
+
+if __name__ == "bob" or __name__ == "__main__":
+    mY_GoOd_fUNCtiON(LetTerS, LetTerS, LetTerS, LetTerS, LetTerS, LetTerS, LetTerS)
